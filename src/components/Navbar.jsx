21a129bda101cb678feaeabd81c6ad7b35958233
jpcarta16 <jpcarta16@gmail.com>
2025-02-04 11:36:26 +0100

    changes with CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Navbar = ({ activeSection }) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <a className="navbar-brand" href="#home">Juan Cardenas</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
